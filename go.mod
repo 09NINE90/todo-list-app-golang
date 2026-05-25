a7de87c6a732a8b0a1d161293be7bdb723ab4f6b
09NINE90 <razumov.aleksei101@yandex.ru>
2026-05-25 16:42:26 +0500

    init_2

--- go.mod
+++
@@ -1,5 +0,0 @@
-module todo-list-golang
-
-go 1.26
-
-require github.com/google/uuid v1.6.0 // indirect
---
+++ migrations/000001_init.up.sql
@@ -0,0 +1,31 @@
+CREATE SCHEMA todoapp;
+
+CREATE TABLE todoapp.users
+(
+    id           uuid PRIMARY KEY,
+    first_name   VARCHAR(50) NOT NULL CHECK (char_length(first_name) BETWEEN 3 AND 50),
+    last_name    VARCHAR(50) NOT NULL CHECK (char_length(last_name) BETWEEN 3 AND 50),
+    phone_number VARCHAR(15) NOT NULL CHECK (
+        phone_number ~ '^\+[0-9]+$'
+            AND
+        char_length(phone_number) BETWEEN 10 AND 15
+        )
+);
+
+CREATE TABLE todoapp.tasks
+(
+    id           uuid PRIMARY KEY,
+    title        VARCHAR(50) NOT NULL CHECK (char_length(title) BETWEEN 3 AND 50),
+    description  VARCHAR(1000) CHECK (char_length(title) BETWEEN 5 AND 1000),
+    completed    BOOLEAN     NOT NULL DEFAULT FALSE,
+    created_at   timestamptz NOT NULL DEFAULT NOW(),
+    completed_at timestamptz,
+
+    CHECK (
+        (completed = FALSE AND completed_at IS NULL)
+            OR
+        (completed = TRUE AND completed_at IS NOT NULL AND completed_at >= created_at)
+        ),
+
+    author_id    uuid        NOT NULL REFERENCES todoapp.users (id)
+);
